eea1ad1f20fa57eb52b888e6bbc2f7448dc3bd76
HalyshAnton <kyhar007@gmail.com>
2024-06-06 10:40:43 +0300

    add example file to folder

---
+++ libraries_examples/easyocr_example.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+import easyocr
+
+
+# read image and resize
+img = Image.open('data/handwriting2.jpg')
+img = img.resize((256, 256))
+
+img.show()
+
+img = np.array(img)
+
+
+# ocr
+reader = easyocr.Reader(['en'])
+results = reader.readtext(img)
+
+text = ' '.join(result[-2] for result in results)
+print(text)
